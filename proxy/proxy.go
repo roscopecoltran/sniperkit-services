7b047d9821afd1e1ca28313e4ec0527cc8a69d1e
Rosco Pecoltran <username@users.noreply.github.com>
2017-08-29 01:52:37 +0200

    Squashed 'krakend/core/' content from commit 946ba2f
    git-subtree-dir: krakend/core
    git-subtree-split: 946ba2fb592307d7729f2871e2b718655fbb7511

---
+++ proxy/proxy.go
@@ -0,0 +1,52 @@
+//Package proxy provides proxy and proxy middleware interfaces and implementations.
+package proxy
+
+import (
+	"context"
+	"errors"
+
+	"github.com/devopsfaith/krakend/config"
+)
+
+// Response is the entity returned by the proxy
+type Response struct {
+	Data       map[string]interface{}
+	IsComplete bool
+}
+
+var (
+	// ErrNoBackends is the error returned when an endpoint has no backends defined
+	ErrNoBackends = errors.New("all endpoints must have at least one backend")
+	// ErrTooManyBackends is the error returned when an endpoint has too many backends defined
+	ErrTooManyBackends = errors.New("too many backends for this proxy")
+	// ErrTooManyProxies is the error returned when a middleware has too many proxies defined
+	ErrTooManyProxies = errors.New("too many proxies for this proxy middleware")
+	// ErrNotEnoughProxies is the error returned when an endpoint has not enough proxies defined
+	ErrNotEnoughProxies = errors.New("not enough proxies for this endpoint")
+)
+
+// Proxy processes a request in a given context and returns a response and an error
+type Proxy func(ctx context.Context, request *Request) (*Response, error)
+
+// BackendFactory creates a proxy based on the received backend configuration
+type BackendFactory func(remote *config.Backend) Proxy
+
+// Middleware adds a middleware, decorator or wrapper over a collection of proxies,
+// exposing a proxy interface.
+//
+// Proxy middlewares can be stacked:
+//	var p Proxy
+//	p := EmptyMiddleware(NoopProxy)
+//	response, err := p(ctx, r)
+type Middleware func(next ...Proxy) Proxy
+
+// EmptyMiddleware is a dummy middleware, useful for testing and fallback
+func EmptyMiddleware(next ...Proxy) Proxy {
+	if len(next) > 1 {
+		panic(ErrTooManyProxies)
+	}
+	return next[0]
+}
+
+// NoopProxy is a do nothing proxy, useful for testing
+func NoopProxy(_ context.Context, _ *Request) (*Response, error) { return nil, nil }
