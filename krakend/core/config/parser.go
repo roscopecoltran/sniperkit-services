ca279fab190e8821d716ad178d0f951c1f86e4b6
Rosco Pecoltran <username@users.noreply.github.com>
2017-08-29 01:54:39 +0200

    remove krakend

--- krakend/core/config/parser.go
+++
@@ -1,6 +0,0 @@
-package config
-
-// Parser reads a configuration file, parses it and returns the content as an init ServiceConfig struct
-type Parser interface {
-	Parse(configFile string) (ServiceConfig, error)
-}
