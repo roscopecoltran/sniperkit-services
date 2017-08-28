5629d0fbe9aab496e0246f11cec1998e8e7b21b4
Rosco Pecoltran <username@users.noreply.github.com>
2017-08-28 19:35:41 +0200

    Squashed 'dockerfiles/telekibi/' content from commit 1bf7f8d89
    git-subtree-dir: dockerfiles/telekibi
    git-subtree-split: 1bf7f8d895dc56be7ad89fb132b59c600fe68a2d

---
+++ entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+if [ "$1" = 'docker-kibi' ]; then
+    if [ "$ELASTICSEARCH_URL" -o "$ELASTICSEARCH_PORT_9200_TCP" ]; then
+        : ${ELASTICSEARCH_URL:='http://elasticsearch:9200'}
+        sed -ri "s!^(\#\s*)?(elasticsearch\.url:).*!\2 '$ELASTICSEARCH_URL'!" /opt/kibi/config/kibi.yml
+    else
+        "No ES URL parameter, starting local instance... "
+        service elasticsearch start
+        sleep 5
+    fi
+else
+        service elasticsearch start
+        sleep 5
+fi
+
+# Patch demo kibi to use standard ES port
+perl -p -i -e "s/9220/9200/" /opt/kibi/kibi/config/kibi.yml
+perl -p -i -e "s/localhost/0.0.0.0/" /opt/kibi/kibi/config/kibi.yml
+
+# Start Kibi
+/opt/kibi/bin/kibi &
+tail -f /var/log/elasticsearch/*.log
+Contact GitHub API Training Shop Blog About
